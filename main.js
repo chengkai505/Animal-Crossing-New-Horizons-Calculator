6233195d2c35f6fdbbb0f9b52ad8ca905473871e
Kai Wu <wuvincentck505@gmail.com>
2020-04-22 15:13:39 +0800

    更新

--- main.js
+++ main.js
@@ -113,7 +113,7 @@ xhr.addEventListener("load", function () {
                 if (obj["名稱"] == target) {
                     option.selected = true;
                     price.readOnly = target == "大頭菜" ? false : true;
-                    price.value = target == "大頭菜" ? "" : Math.floor(obj["售價"] * (doublePrice == target ? 2 : modifier));
+                    price.value = target == "大頭菜" ? "" : Math.floor(obj["價格"] * (doublePrice == target ? 2 : modifier));
                     quantity.placeholder = target == "大頭菜" ? "單位：顆" : "";
                 }
                 item.appendChild(option);
@@ -155,7 +155,7 @@ xhr.addEventListener("load", function () {
             subtotal.innerText = "0";
             if (e.target.value != "NULL") {
                 price.readOnly = e.target.value == "大頭菜" ? false : true;
-                price.value = e.target.value == "大頭菜" ? "" : Math.floor(data[index[e.target.value]["type"]][index[e.target.value]["index"]]["售價"] * (doublePrice == e.target.value ? 2 : modifier));
+                price.value = e.target.value == "大頭菜" ? "" : Math.floor(data[index[e.target.value]["type"]][index[e.target.value]["index"]]["價格"] * (doublePrice == e.target.value ? 2 : modifier));
                 quantity.placeholder = e.target.value == "大頭菜" ? "單位：顆" : "";
             }
             break;
@@ -275,7 +275,7 @@ function priceUpdate() {
         if (type.value == "NULL" || item.value == "大頭菜") {
             continue;
         }
-        price.value = Math.floor(data[type.value][index[item.value]["index"]]["售價"] * (modifier == 1 && doublePrice == item.value ? 2 : modifier));
+        price.value = Math.floor(data[type.value][index[item.value]["index"]]["價格"] * (modifier == 1 && doublePrice == item.value ? 2 : modifier));
         subtotal.value = parseInt(quantity.value != "" ? quantity.value : 0) * price.value;
         totalUpdate();
     }
